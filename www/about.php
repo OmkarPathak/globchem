7477288fac1143784a02b881939ac3526516c850
Omkar Pathak <omkarpathak27@gmail.com>
2020-10-02 17:28:59 +0530

    Added about page

--- www/about.php
+++ www/about.php
@@ -24,29 +24,37 @@ include('header.php')
             <div class="container" data-aos="fade-up">
 
                 <div class="row no-gutters">
-                    <div class="col-lg-6 video-box">
-                        <img src="assets/img/about.jpg" class="img-fluid" alt="">
-                        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                    <div class="col-lg-5 video-box">
+                        <img src="assets/img/about_1.jpg" class="img-fluid" alt="">
+                        <a href="https://www.youtube.com/watch?v=SPUqjXlnLJQ&feature=emb_title" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
                     </div>
 
-                    <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
+                    <div class="col-lg-7 d-flex flex-column justify-content-center about-content">
 
                         <div class="section-title">
                             <h2>About Us</h2>
                             <h3>Find Out More <span>About Us</span></h3>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
+                            <p> Globe Construction Chemicals was founded in 2015 is An ISO 9001:2015 certified
+                                company is a professionally managed young company engaged into
+                                manufacturing of construction chemicals. The company has its manufacturing unit
+                                located in Pune &amp; Gujrat having advance-manufacturing process with stringent
+                                quality control.Globe is a leader in the delivery of tailored Constructive Solutions
+                                for virtually any type of construction project, combining high-quality products,
+                                expert technical support, customer service and innovation.
+                            </p>
                         </div>
 
                         <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
                             <div class="icon"><i class="bx bx-fingerprint"></i></div>
                             <h4 class="title"><a href="">Our Visions</a></h4>
-                            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                            <p class="description">Our mission is to be the leading supplier of superior construction chemical by consistently improving the quality of our product to add value for clients through innovation, foresight, integrity, and aggressive performance; we bring
+                            advancement, feel and supportability to construction.</p>
                         </div>
 
                         <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
                             <div class="icon"><i class="bx bx-gift"></i></div>
                             <h4 class="title"><a href="">Our Mission</a></h4>
-                            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                            <p class="description">Our vision is to offer innovative product to construction industries which help to our customers to achieve sustainable building practice.</p>
                         </div>
                     </div> 
                 </div>
@@ -67,8 +75,12 @@ include('header.php')
                         <img src="assets/img/about-1.jpg" class="img-fluid" alt=""> 
                     </div>
                     <div class="col-lg-8 col-md-8 icon-box" data-aos="fade-up" data-aos-delay="100">
-                        <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br><br>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeatUlamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br><br>
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat<br><br></p>
+                        <p class="description" style="font-size: 18px;">Globe Construction Chemicals was founded in 2015.<br>Globe Construction Chemicals have been producing innovative construction chemicals that help gives strength and increase the life span of a modern building.<br>
+                        At the moment and we supply high-quality products to major construction
+                        companies and real estate projects.From our concrete admixtures, Masonary
+                        mortar, Readymix Plaster, Tile Adhesive, flooring products, expansion joints,
+                        structural grout and our extensive range of waterproofing products, Globe
+                        Construction Chemicals has all your construction needs covered.<br>Our products are available through our network of authorized distributors and partners.</p>
                     </div>
                     <!-- <div class="col-lg-2 col-md-2 icon-box" data-aos="fade-up" data-aos-delay="200">
                     </div> -->
@@ -79,43 +91,46 @@ include('header.php')
         <!-- ======= About Lists Section ======= -->
         <section class="about-lists">
             <div class="container">
-
+                <div class="section-title">
+                    <h2>Our Values</h2>
+                    <!-- <h3>From where it all <span>Begins</span></h3> -->
+                </div>
                 <div class="row no-gutters">
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up">
                         <span>01</span>
-                        <h4>Quality Products</h4>
-                        <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+                        <h4>Customer First</h4>
+                        <p>We develop relationship that make a positive difference in our customer lives</p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="100">
                         <span>02</span>
-                        <h4>Fine Design</h4>
-                        <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+                        <h4>Efficacy</h4>
+                        <p>To produce a desired result</p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="200">
                         <span>03</span>
-                        <h4>Quick Delivery</h4>
-                        <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+                        <h4>Integrity</h4>
+                        <p>Be real and authentic</p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="300">
                         <span>04</span>
-                        <h4>Cost Effective</h4>
-                        <p>Inventore quo sint a sint rerum. Distinctio blanditiis deserunt quod soluta quod nam mider lando casa</p>
+                        <h4>Resposibility</h4>
+                        <p>Courage to take on big challenges and see them through</p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="400">
                         <span>05</span>
-                        <h4>ISO 9001:2015</h4>
-                        <p>Vitae dolorem in deleniti ipsum omnis tempore voluptatem. Qui possimus est repellendus est quibusdam</p>
+                        <h4>Quality</h4>
+                        <p>What we do, we do well</p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="500">
                         <span>06</span>
-                        <h4>High Standards</h4>
-                        <p>Quis eum numquam veniam ea voluptatibus voluptas. Excepturi aut nostrum repudiandae voluptatibus corporis sequi</p>
+                        <h4>Cost Effective</h4>
+                        <p>At a very reasonable price</p>
                     </div>
                 </div>
             </div>
